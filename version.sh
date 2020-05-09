2e0a7a5947a37c71a15077cdd8009a1dede50c4c
radko <justslavic@gmail.com>
2020-05-09 09:57:52 +0300

    Update version.sh

--- version.sh
+++ version.sh
@@ -5,16 +5,13 @@ regex='version_([0-9]+)\.([0-9]+)\.([0-9]+)(-[0-9]+)?(-.*)?'
 
 [[ "$version" =~ $regex ]]
 
-echo "v.${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.${BASH_REMATCH[3]}-${BASH_REMATCH[4]:1}-${BASH_REMATCH[5]:1}"
-# echo "version: (${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.${BASH_REMATCH[3]})"
-# echo "build:   ${BASH_REMATCH[4]:1}"
-# echo "hash:    ${BASH_REMATCH[5]:1}"
-
 major="${BASH_REMATCH[1]}"
 minor="${BASH_REMATCH[2]}"
 patch="${BASH_REMATCH[3]}"
 build="${BASH_REMATCH[4]:1}"
 hash="${BASH_REMATCH[5]:1}"
+
+echo "v.$major.$minor.$patch-$build-$hash"
 
 cat > src/version.cpp <<- EOM
 #include <version.h>
