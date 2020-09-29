ba6ca1ccc47388f99d61896915c394bab9479177
radko <justslavic@gmail.com>
2020-09-29 23:55:52 +0300

    Update defines and assertions

--- include/defines.h
+++ include/defines.h
@@ -8,22 +8,39 @@
 #include <utils.h>
 #include <logging/logging.h>
 
+#define STRINGIFY2(X) #X
+#define STRINGIFY(X) STRINGIFY2(X)
 
+#ifdef DEBUG
 #define ASSERT(x) \
-    if (!(x)) { \
-        LOG_ERROR << "Failed at " << __FILE__ << ':' << __LINE__; \
+    if (x) { } else { \
+        LOG_ERROR << "Assertion failed at " __FILE__ ":" STRINGIFY(__LINE__); \
         std::exit(1); \
     } void SEMICOLON__()
 
-#define GL_CALL(call) \
-    call; \
-    ASSERT(check_gl_errors())
+#define ASSERT_EQ(x, y) \
+    if (x == y) {} \
+    else { \
+        LOG_ERROR << "Assertion failed at " __FILE__ ":" STRINGIFY(__LINE__); \
+    } void SEMICOLON__()
 
-#define GL_CHECK_ERRORS \
-    ASSERT(check_gl_errors())
+#define ASSERT_NE(x, y) \
+    if (x == y) { \
+        LOG_ERROR << "Assertion failed at " __FILE__ ":" STRINGIFY(__LINE__); \
+    } else {} void SEMICOLON__()
 
-#define STRINGIFY2(X) #X
-#define STRINGIFY(X) STRINGIFY2(X)
+#define GL_CALL(call) call; ASSERT(check_gl_errors())
+#define GL_CHECK_ERRORS ASSERT(check_gl_errors())
+
+#else
+
+#define ASSERT(x)
+#define ASSERT_EQ(x, y)
+#define ASSERT_NE(x, y)
+#define GL_CALL(call) call
+#define GL_CHECK_ERRORS
+
+#endif
 
 
 typedef int8_t   i8;
@@ -41,7 +58,7 @@ typedef double   f64;
 
 enum GIR1_ERROR {
     ERROR_GLFW_FAILED = 256,
-    ERROT_GLEW_FAILED = 257,
+    ERROR_GLEW_FAILED = 257,
 };
 
 #endif // GIR1_DEFINES_H
